dcfec12604efe12fa0e97ba87c9626a7c5136b93
Yormen <yormen1@gmail.com>
2024-10-23 17:51:23 +0000

    updated the url

--- lib/config/soup_market_config.dart
+++ lib/config/soup_market_config.dart
@@ -7,7 +7,7 @@ class SoupMarketConfig {
 
   SoupMarketConfig({
     required this.apiKey,
-    this.baseUrl = 'https://api.soupmarket.com',
+    this.baseUrl = 'https://demo.soupmarkets.com',
     this.environment = Environment.sandbox,
     this.timeout = const Duration(seconds: 30),
     this.additionalHeaders,
@@ -21,7 +21,7 @@ enum Environment {
   String get baseUrl {
     switch (this) {
       case Environment.sandbox:
-        return 'https://sandbox-api.soupmarket.com';
+        return 'https://demo.soupmarkets.com/login/auth';
       case Environment.production:
         return 'https://api.soupmarket.com';
     }
