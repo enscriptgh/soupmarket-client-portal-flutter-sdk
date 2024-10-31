8c936f87213aca96cd16e7b20680c9f2dd814bcf
Yormen <yormen1@gmail.com>
2024-10-31 16:02:43 +0000

    commented out the environment created to used the parsed baseUrl

--- lib/config/soup_market_config.dart
+++ lib/config/soup_market_config.dart
@@ -2,7 +2,7 @@ class SoupMarketConfig {
   final String apiKey;
   final String apiSecret;
   final String baseUrl;
-  final Environment environment;
+  // final Environment environment;
   final Duration timeout;
   final Map<String, dynamic>? additionalHeaders;
 
@@ -10,7 +10,7 @@ class SoupMarketConfig {
     required this.apiKey,
     required this.apiSecret,
     this.baseUrl = 'https://demo.soupmarkets.com',
-    this.environment = Environment.sandbox,
+    // this.environment = Environment.sandbox,
     this.timeout = const Duration(seconds: 30),
     this.additionalHeaders,
   });
