31a4d23eaf2b0cf7404511302d30b2c2eb7900f5
Yormen <yormen1@gmail.com>
2024-11-01 21:00:34 +0000

    added risk answer and added filter to be parsed

--- lib/utils/const_api_urls.dart
+++ lib/utils/const_api_urls.dart
@@ -39,25 +39,25 @@ final String FUND_UNDER_MANAGEMENT = "/security/index.json?enableFundManagement=
 final String ORDER_REQUEST_STATUS = "/client/orderRequestStatus/";
 
 final String CREATE_ORDER_REQUEST = "/client/orderRequest";
-final String CLIENT_CUSTODY_STATEMENT = "/client/custodyStatement.json?contact=";
-final String ORDER_REQUEST_HISTORY = "/client/orderRequestHistory.json?contact=";
+final String CLIENT_CUSTODY_STATEMENT = "/client/custodyStatement";
+final String ORDER_REQUEST_HISTORY = "/client/orderRequestHistory";
 
 final String SAVE_ACCOUNT_SERVICE = "/client/createAccountServices";
 final String UPDATE_ACCOUNT_SERVICE = "/client/updateAccountServices";
 final String CHECK_DEPOSIT_AND_WITHDRAWAL_PROCESS = "/client/accountServices";
 
 final String PIN_RESET_QUESTIONS = "/pinResetQuestion/index";
-final String REQUEST_PIN_RESET = "/client/pinReset.json?contact=";
-final String CONFIRM_PIN_RESET = "/client/pinReset.json?contact=";
-final String CHANGE_PIN_RESET = "/client/changePin.json?contact=";
-final String CREATE_PIN_RESET = "/client/createPin.json?contact=";
+final String REQUEST_PIN_RESET = "/client/pinReset";
+final String CONFIRM_PIN_RESET = "/client/pinReset";
+final String CHANGE_PIN_RESET = "/client/changePin";
+final String CREATE_PIN_RESET = "/client/createPin";
 
 //Security & Asset Aloocation
 final String CREATE_SECURITY_SUB_TYPE_ASSET_ALLOCATION = "/client/createSecuritySubTypeAssetAllocation";
 final String SECURITY_FLOWS = "/flow/index";
 final String SECURITY_ASSET_ALLOCATION = "/client/createSecurityAssetAllocation";
 
-final String SEND_SMS_MESSAGE = "/sendMessage/sendSms.json?smsNumber=";
+final String SEND_SMS_MESSAGE = "/sendMessage/sendSms";
 
 final String LOAD_ALL_ANNUAL_GROSS_INCOME = "/annualGrossIncome/index";
 final String LOAD_ALL_MONTHLY_INCOME = "/monthlyGrossIncome/index";
@@ -66,7 +66,7 @@ final String LOAD_ENUM_UTILITIES = "/enumUtility/index";
 final String LOAD_ANNUAL_TURN_OVER = "/annualTurnOver/index";
 
 final String SAVE_CLIENT_BANK_DETAILS = "/clientBankDetails/save";
-final String UPDATE_CLIENT_BANK_DETAILS = "/clientBankDetails/update/";
+final String UPDATE_CLIENT_BANK_DETAILS = "/clientBankDetails/update";
 
 //Downloadable Reports
 final String CUSTODY_STATEMENT = "/tradeReports/custodyStatement";
@@ -79,3 +79,4 @@ final String SAVE_CLIENT_EMERGENCY_CONTACT = "/clientEmergencyContact/save";
 final String UPDATE_CLIENT_EMERGENCY_CONTACT = "/clientEmergencyContact/update";
 
 final String LOAD_ALL_RISK_ASSESSMENT_QUESTIONS = "/riskAssessmentQuestion/index";
+final String LOAD_ALL_RISK_ASSESSMENT_ANSWERS = "/riskAssessmentAnswer/index";
